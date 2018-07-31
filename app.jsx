cecdd45632d07e126b92f62ff255ef02b48ef3e1
Jannes Brunner <jannes.brunner@student.htw-berlin.de>
2018-07-31 16:36:13 +0100

    Building the Statistics Component

--- app.jsx
+++ app.jsx
@@ -24,6 +24,7 @@ let PLAYERS = [
 function Header(props) {
     return (
         <div className="header">
+            <Stats players={props.players} />
             <h1>{props.title}</h1>
         </div>
     );
@@ -31,6 +32,7 @@ function Header(props) {
 
 Header.propTypes = {
     title: React.PropTypes.string.isRequired,
+    players: React.PropTypes.array.isRequired,
 }
 
 function Player(props) {
@@ -74,6 +76,38 @@ Counter.propTypes = {
     onChange: React.PropTypes.func.isRequired,
 }
 
+const Stats = (props) => {
+    let totalPlayers = props.players.length;
+    let totalPoints = props.players.reduce((total, player) => {
+        return total + player.score;
+    }, 0);
+    // Other way:
+    // let totalPoints = 0;
+    // props.players.forEach((player) => {
+    //     totalPoints += player.score;
+    // });
+
+    return (
+        <table className="stats">
+            <tbody>
+                <tr>
+                    <td>Players:</td>
+                    <td>{totalPlayers}</td>
+                </tr>
+                <tr>
+                    <td>Total Points:</td>
+                    <td>{totalPoints}</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+Stats.propTypes = {
+    players: React.PropTypes.array.isRequired,
+
+}
+
 class Application extends React.Component {
 
     constructor(props) {
@@ -92,7 +126,7 @@ class Application extends React.Component {
     render() {
         return (
             <div className="scoreboard">
-                <Header title={this.props.title} />
+                <Header title={this.props.title} players={this.state.players} />
                 <div className="players">
                     {this.state.players.map(function (player, index) {
                         return (
