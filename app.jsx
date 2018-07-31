eea2892dc2b5e478ddb4ff85c96791a0910dea82
Jannes Brunner <jannes.brunner@student.htw-berlin.de>
2018-07-31 11:55:34 +0100

    Make Counter class component

--- app.jsx
+++ app.jsx
@@ -56,21 +56,26 @@ Player.defaultProps = {
     score: 0,
 }
 
-function Counter(props) {
-    return (
-        <div className="counter">
-            <button className="counter-action decrement"> - </button>
-            <div className="counter-score"> {props.score} </div>
-            <button className="counter-action increment"> + </button>
-        </div>
-    );
+class Counter extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="counter" >
+                <button className="counter-action decrement"> - </button>
+                <div className="counter-score"> {this.props.score} </div>
+                <button className="counter-action increment"> + </button>
+            </div>
+        );
+    }
+
 }
 
 Counter.propTypes = {
     score: React.PropTypes.number.isRequired
 }
-
-
 
 
 function Application(props) {
